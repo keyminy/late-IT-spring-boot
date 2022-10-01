9132caf62201e47f5fb889e8a88e12167fda4570
keyminy <imanaifs@gmail.com>
2022-10-02 01:34:01 +0900

    ajax-back

--- src/main/java/com/example/coffee/controller/CoffeeV1.java
+++ src/main/java/com/example/coffee/controller/CoffeeV1.java
@@ -2,13 +2,13 @@ package com.example.coffee.controller;
 
 import com.example.coffee.service.CoffeeV1Service;
 import lombok.extern.log4j.Log4j2;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
@@ -28,6 +28,34 @@ public class CoffeeV1 {
         model.addAttribute("list",list);
        //System.out.println(list);
         return "/v1/coffee";
+    }
+
+    @GetMapping("/coffeeAjax")
+    @ResponseBody
+    public String doCoffeeAjax(Model model){
+        return "/v1/coffeeAjax";
+    }
+    @PostMapping("/coffeeAjaxPost")
+    @ResponseBody
+    public String doCoffeeAjaxPost(HttpServletRequest request){
+        log.info("=== doCoffeePostAjax ===");
+        String strName = request.getParameter("name");
+        String strStart_date = request.getParameter("start_date");
+        String strEnd_date = request.getParameter("end_date");
+        String strKind = request.getParameter("kind");
+        log.info("strKind : " + strKind);
+
+        /* 전체 리스트 조회 - 오버로딩 */
+        List<Map<String,String>> list =  v1Service.doCoffeeList(strStart_date,strEnd_date,strName,strKind);
+
+        JSONArray jsonArray = new JSONArray();
+        for(Map<String,String> fMap : list){
+            JSONObject resObj = new JSONObject(fMap); //map을 JSON {  } 한 줄 만들어줌
+            jsonArray.put(resObj); //JsonArray에 { } 한 줄 씩 array에 넣어준다.
+        }
+        JSONObject object = new JSONObject();
+        object.put("coffee_list",jsonArray);
+        return object.toString();
     }
 
     @PostMapping("/coffee")
